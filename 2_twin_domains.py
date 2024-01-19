5a2715d3d24bf99c255cd65b1c65e6d8e122dd57
Bing Li <libing621@gmail.com>
2024-01-19 10:53:13 -0600

    updated tutorials

--- 2_twin_domains.py
+++ 2_twin_domains.py
@@ -13,12 +13,12 @@ if __name__ == "__main__":
     # generating multiple domains of the given cell by rotations
     domains = Domains(cell)
     domains.make_twin(rot_angles=180, rot_axes=cell.c_vec, origin=cell.b_vec)
-    domains.make_twin(rot_angles=180, rot_axes=cell.a_vec, origin=cell.b_vec)
+    # domains.make_twin(rot_angles=180, rot_axes=cell.a_vec, origin=cell.b_vec)
     domains.plot_domains()
 
     # generate Bragg peaks, defalut d_min=1
     domains.plot_peaks(projection="H,0,L", del_Q=0.1, d_min=1)
-    domains.plot_peaks(projection="H,K,1", del_Q=0.2)
-    domains.plot_peaks(projection="0,K,L", del_Q=0.3)
+    domains.plot_peaks(projection="H,K,0", del_Q=0.5)
+    domains.plot_peaks(projection="0,K,L", del_Q=0.5)
 
     plt.show()
